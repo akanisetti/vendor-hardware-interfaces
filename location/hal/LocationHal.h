5bca36099d233df76c964ff3b8ce097ed4612154
Anisetti Avinash Krishna <anisetti.avinash.krishna@intel.com>
2026-04-20 09:28:37 +0000

    Add Intel static location HAL
    Add new location HAL module implementing vendor AIDL interface for static
    location coordinate provisioning. Includes HAL daemon with configuration
    file parsing, binder service registration, and location coordinate access.
    
    Tracked-On: NIACP3-1182
    Signed-off-by: Anisetti Avinash Krishna <anisetti.avinash.krishna@intel.com>

---
+++ location/hal/LocationHal.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2026 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <aidl/vendor/intel/location/BnLocationHal.h>
+
+#include <mutex>
+#include <string>
+
+namespace aidl::vendor::intel::location {
+
+class LocationHal final : public BnLocationHal {
+public:
+    explicit LocationHal(const std::string& configPath);
+
+    ndk::ScopedAStatus getLatitude(double* _aidl_return) override;
+    ndk::ScopedAStatus getLongitude(double* _aidl_return) override;
+
+private:
+    void readConfig();
+
+    const std::string mConfigPath;
+    mutable std::mutex mMutex;
+    double mLatitude = 0.0;
+    double mLongitude = 0.0;
+};
+
+}  // namespace aidl::vendor::intel::location
